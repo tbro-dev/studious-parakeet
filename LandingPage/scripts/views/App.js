1dfb568044e28010d7d26ae941d7ca64e400702e
tylerjbroussard@wavfox.com <tylerjbroussard@wavfox.com>
2021-08-08 09:30:29 -0700

    random push

--- LandingPage/scripts/views/App.js
+++ LandingPage/scripts/views/App.js
@@ -1,5 +1,7 @@
 //https://nodejs.org/en/knowledge/HTTP/servers/how-to-serve-static-files/
 
+//https://material-ui.com/premium-themes/onepirate/
+
 const { render } = ReactDOM;
 
 render(<h1>Hello, world!</h1>, document.getElementById("root"));
